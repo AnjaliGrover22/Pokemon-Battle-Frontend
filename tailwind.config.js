9757427bd7a6b88ce278e89f558a89b64f47d525
Deancs1 <carmel.dean+GitHub@gmail.com>
2024-09-16 12:04:31 +0200

    fixed error issue and moved css to index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
       colors: {
-        "custom-gray": "#222222",
-
+        "custom-gray": "#222222", // Closing bracket was missing here
+      },
       dropShadow: {
         "glow-red": "0 0 200px rgba(255, 0, 0, 0.7)", // Red glow
         "glow-yellow": "0 0 200px rgba(255, 255, 0, 0.7)", // Yellow glow
-
       },
     },
   },
